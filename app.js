3fb0eb213df62390b802b6cbf75737dc247da4f2
PWDTLombardi <t.lombardi@baypath.net>
2020-05-10 13:44:05 -0400

    i have the image loaded but now i need to cut them up individually in photoshop and change my code around

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-var express = require("express");
-var app = express();
-var path = require("path");
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
+var express = require('express'),
+    path = require('path'),
+    app = express();
+
+app.use(express.static('public')); // enables access to the public folder which has the images
+
+// Express Middleware for serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/index.html',function(req,res){
+   res.sendFile(path.join(__dirname + '/index.html')); 
 });
+
+app.get('/js/three.js',function(req,res){
+    res.sendFile(path.join(__dirname + '/js/three.js')); 
+});
+
+app.get('/js/TrackballControls.js',function(req,res){
+    res.sendFile(path.join(__dirname + '/js/TrackballControls.js')); 
+});
+
+
+app.get('/', function(req, res) {
+    res.redirect('index.html');
+});
+
 app.listen(3000);
 console.log("Server running at Port 3000");
